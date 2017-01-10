fe40d1ca18df6b5d63e24d124cc511c46c53b526
santiagomb <santiago.m92@hotmail.com>
2017-01-10 15:48:19 -0300

    Primeros tests simples

--- APIrest/grails-app/controllers/apirest/ArticleController.groovy
+++ APIrest/grails-app/controllers/apirest/ArticleController.groovy
@@ -1,30 +1,42 @@
 package apirest
 
 import grails.converters.JSON
+import grails.rest.RestfulController
 
 import javax.servlet.http.HttpServletResponse
 
-class ArticleController {
+class ArticleController extends RestfulController<Article> {
 
+    static responseFormats = ['json']
+
+    ArticleController(){
+        super(Article)
+    }
+
+    @Override
     def index() {
         render status: 200, text: 'Article API running OK.'
     }
 
+    @Override
     def show() {
         render Article.list() as JSON
     }
 
+    @Override
     def create() {
         Article a = new Article(params)
         a.save()
         render a as JSON
     }
 
+    @Override
     def delete(){
         Article.get(params.id)?.delete()
         render status: HttpServletResponse.SC_NO_CONTENT
     }
 
+    @Override
     def update() {
         Article a = Article.get(params.id)
         a.properties = params
--- APIrest/test/unit/apirest/ArticleControllerSpec.groovy
+++ APIrest/test/unit/apirest/ArticleControllerSpec.groovy
@@ -1,5 +1,6 @@
 package apirest
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,14 +8,22 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ArticleController)
+@Mock(Article)
 class ArticleControllerSpec extends Specification {
 
     def setup() {
+        new Article(title: "Título de test", contenido: "Esto es un testeo",
+                autor: "Admin", visitas: 0, comentarios: ["Testing1", "Testing2"]).save()
     }
 
     def cleanup() {
     }
 
     void "test something"() {
+        when:
+            controller.show()
+
+        then:
+            Article.list().size() == 1
     }
 }
